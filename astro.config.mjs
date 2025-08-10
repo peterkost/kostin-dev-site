a4f9d5db9ebb556e45e70b6748312b5e0210fc77
Peter Kostin <46546031+peterkost@users.noreply.github.com>
2025-08-10 15:07:52 -0600

    feat: add stying from kostin.life

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
+      enabled: true,
     },
 
-    imageService: "cloudflare"
-  })
+    imageService: "cloudflare",
+  }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
+
