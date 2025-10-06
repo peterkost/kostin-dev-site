1463f8d56626999594a92fd14f1c09a03ea5a1e1
Peter Kostin <46546031+peterkost@users.noreply.github.com>
2025-10-06 13:10:27 +0700

    build: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   redirects: {
     "/on-devex": "/blog/on-developer-experience",
   },
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
@@ -15,7 +18,10 @@ export default defineConfig({
 
     imageService: "cloudflare",
   }),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
